15728ab3b1fc710222088cab621f30eae714f766
laptop <ten_mail_nie_dziala>
2025-07-31 10:40:04 +0200

    AAAAAAAAAAAAAAAAAA

--- react-pages-project/src/components/pages/state_persistence/state_persistence.tsx
+++ react-pages-project/src/components/pages/state_persistence/state_persistence.tsx
@@ -1,10 +1,9 @@
-// import style from './redux_persists.module.css'
 import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { increment, decrement, reset } from './../../../counterSlice';
 
 function state_persistence()
 {
-    const [count_2, setCount_2] = useState<number>(0);
-
     const [count_1, setCount_1] = useState<number>(() => {
         const storedValue = localStorage.getItem('count_1');
         if (storedValue !== null) {
@@ -20,23 +19,49 @@ function state_persistence()
     });
     useEffect(() => {localStorage.setItem('count_1', JSON.stringify(count_1));}, [count_1]);
 
+    const [count_2, setCount_2] = useState<number>(0);
+
+    const count_3 = useSelector((state: any) => state.counter.count);
+    const dispatch = useDispatch();
 
     return(
         <>
             <title>State Persistence Test</title>
 
             <header>
-                <h1>State Persistence Test</h1>
+                <h1 style={{textAlign : "center"}}>State Persistence Test</h1>
             </header>
 
             <hr />
 
-            <main style={{display : 'flex' , flexDirection : "row"  , justifyContent : "center"  , alignItems : "center" , gap : 1 + "rem"}}>
-                With State Persistence -{'>'}
-                <button style={{width : 30 + "%" , height : 4 + "rem"}} onClick={() => setCount_1(count_1 + 1)}>{count_1}</button>
+            <main>
+                <h2 style={{textAlign : "center"}}>No State Persistence</h2>
+                <h5 style={{textAlign : "center" , width : 100 + "%"}}>Counter 2 : {count_2}</h5>
+                <section style={{display : 'flex' , flexDirection : "row"  , justifyContent : "center"  , alignItems : "center" , gap : 1 + "rem"}}>
+                    <button style={{ width : 25 + "%" , height : 4 + "rem"}} onClick={() => setCount_2(count_2 + 1)}>+1</button>
+                    <button style={{ width : 25 + "%" , height : 4 + "rem"}} onClick={() => setCount_2(count_2 - 1)}>-1</button>
+                    <button style={{ width : 25 + "%" , height : 4 + "rem"}} onClick={() => setCount_2(count_2 - count_2)}>Reset</button>
+                </section>
+
+                <hr />
+
+                <h2 style={{textAlign : "center"}}>Basic State Persistence</h2>
+                <h5 style={{textAlign : "center" , width : 100 + "%"}}>Counter 1 : {count_1}</h5>
+                <section style={{display : 'flex' , flexDirection : "row"  , justifyContent : "center"  , alignItems : "center" , gap : 1 + "rem"}}>
+                    <button style={{ width : 25 + "%" , height : 4 + "rem"}} onClick={() => setCount_1(count_1 + 1)}>+1</button>
+                    <button style={{ width : 25 + "%" , height : 4 + "rem"}} onClick={() => setCount_1(count_1 - 1)}>-1</button>
+                    <button style={{ width : 25 + "%" , height : 4 + "rem"}} onClick={() => setCount_1(count_1 - count_1)}>Reset</button>
+                </section>
+
+                <hr />
                 
-                <button style={{width : 30 + "%" , height : 4 + "rem"}} onClick={() => setCount_2(count_2 + 1)}>{count_2}</button>
-                {'<'}- Without State Persistence
+                <h2 style={{textAlign : "center"}}>State Persistence with Redux Persist</h2>
+                <h5 style={{textAlign : "center" , width : 100 + "%"}}>Counter 3 : {count_3}</h5>
+                <section style={{display : 'flex' , flexDirection : "row"  , justifyContent : "center"  , alignItems : "center" , gap : 1 + "rem"}}>
+                    <button style={{ width : 25 + "%" , height : 4 + "rem"}} onClick={() => dispatch(increment())}>+1</button>
+                    <button style={{ width : 25 + "%" , height : 4 + "rem"}} onClick={() => dispatch(decrement())}>-1</button>
+                    <button style={{ width : 25 + "%" , height : 4 + "rem"}} onClick={() => dispatch(reset())}>Reset</button>
+                </section>
             </main>
 
             <hr />
---
+++ react-pages-project/src/store.ts
@@ -0,0 +1,27 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import counterReducer from './counterSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['counter'],
+};
+
+const rootReducer = combineReducers({
+  counter: counterReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
